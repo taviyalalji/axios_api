8623baec2b99f3eaf55cb864fcb1a124d1671d18
taviya lalji <taviya2003@gmail.com>
2024-04-21 22:25:26 +0530

    using hooks

--- src/component/Axios_file.jsx
+++ src/component/Axios_file.jsx
@@ -1,15 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios';
 
 function Axios(){
+ // State to store the fetched data
+  const [data, setData] = useState([]);
 
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(res => {
-        console.log(res.data);
-    })
-   return(
-        <></>
-    )
-}
+  // Function to fetch data using Axios
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      console.log(response)
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  // Call fetchData on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Data:</h2>
+           {data.map((post) => (
+        <p>{post.id} : {post.title}<br /></p> 
+        ))}
+     </div>
+  );
+};
 export default Axios
+
